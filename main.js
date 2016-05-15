b129b2fd57215a9f2e3c8054ef5707ff42754572
snuggles08 <iakornfeld@gmail.com>
2016-05-15 14:04:31 -0400

    i did and fixed a bad

---
+++ main.js
@@ -0,0 +1,20 @@
+let ref = require('ref');
+let ffi = require('ffi');
+let Q = require('q');
+
+let arch = require('process').arch;
+
+let libborderlessundertale = ffi.Library('dll/win64.dll', {
+  'MakeWindow': [arch === 'x64' ? 'uint64' : 'uint32', ['string']],
+  'MoveUTWindow': ['void', [arch === 'x64' ? 'uint64' : 'uint32', 'int', 'int']]
+});
+
+let utwindow = null;
+
+Q.fcall(function() {
+  utwindow = libborderlessundertale.MakeWindow('C:\\Users\\snuggles08\\Documents\\UNDERTALE NoSteam\\UNDERTALE.exe');
+})
+.delay(2500)
+.then(function() {
+  libborderlessundertale.MoveUTWindow(utwindow, 50, 50);
+});
